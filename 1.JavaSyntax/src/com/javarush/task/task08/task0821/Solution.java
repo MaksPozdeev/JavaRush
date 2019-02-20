5675155a8684e7f7486b6cb059d355dddb1a4236
Maks Pozdeev <maximglazov18@gmail.com>
2019-02-20 23:26:08 +0300

    level 8 (add tasks 2)

--- 1.JavaSyntax/src/com/javarush/task/task08/task0821/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0821/Solution.java
@@ -16,14 +16,25 @@ public class Solution {
 
     public static Map<String, String> createPeopleList() {
         //напишите тут ваш код
-        Map<String, String> map = new TreeMap<>();
-
-        return null;
+        HashMap<String, String> hashmap = new HashMap<>();
+            hashmap.put("A1", "A");
+            hashmap.put("A2", "B");
+            hashmap.put("A3", "C");
+            hashmap.put("A4", "A");
+            hashmap.put("A2", "D");
+            hashmap.put("A6", "B");
+            hashmap.put("A1", "E");
+            hashmap.put("A8", "F");
+            hashmap.put("A9", "G");
+            hashmap.put("A10", "C");
+        return hashmap;
     }
 
     public static void printPeopleList(Map<String, String> map) {
+//        System.out.println(map.size());
         for (Map.Entry<String, String> s : map.entrySet()) {
             System.out.println(s.getKey() + " " + s.getValue());
+
         }
     }
 }
